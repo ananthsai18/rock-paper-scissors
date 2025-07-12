f77a9cfeb6c440b12253ad702d6aa11f02e987ff
Ananthsai <ananthsaigudla1718@gmail.com>
2025-07-12 15:57:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let count = JSON.parse(localStorage.getItem("Scores")) || {
+  wins: 0,
+  draw: 0,
+  losses: 0,
+};
+updatescore();
+
+function playGame(a) {
+  let result;
+  let userOption = a;
+  const r = Math.floor(Math.random() * 3);
+  const choices = ["Rock", "Paper", "Scissors"];
+  let computerChoice = choices[r];
+  if (userOption === computerChoice) {
+    result = "draw";
+    count.draw += 1;
+  } else if (
+    (userOption === "Rock" && computerChoice === "Scissors") ||
+    (userOption === "Paper" && computerChoice === "Rock") ||
+    (userOption === "Scissors" && computerChoice === "Paper")
+  ) {
+    count.wins += 1;
+    result = " You Won";
+  } else {
+    count.losses += 1;
+    result = "You lost";
+  }
+  localStorage.setItem("Scores", JSON.stringify(count));
+  document.querySelector(".js-result").innerHTML = result;
+  document.querySelector(
+    ".js-move"
+  ).innerHTML = ` You <img class="imgi" src="./images/${userOption}-emoji.png" />
+        <img class="imgi" src="./images/${computerChoice}-emoji.png" alt="" /> Computer`;
+  updatescore();
+}
+function updatescore() {
+  document.querySelector(
+    ".js-score"
+  ).innerHTML = `wins: ${count.wins} losses: ${count.losses} ties: ${count.draw}`;
+}
